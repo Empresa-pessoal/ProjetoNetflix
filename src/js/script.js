25939c81c81e048c813f834b3498d7153396767e
Caio <caio.balassoni1510@gmail.com>
2025-09-03 10:51:43 -0300

    7- adicionando um javascript quebrado

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// DECLARANDO AS VARIAVEIS COM DOM (DOCUMENT MODEL OBJECT)
+const carrosselLista = document.querySelector(".carrossel-lista");
+const carrosselItem = document.querySelectorAll(".carrossel-item");
+const botaoAnterior =document.getElementsByClassName("btn-anterior");
+const botaoProximo =document.getElementsByClassName("btn-proximo");
+
+let contador=0;
+const larguraItem=carrosselItem[0].offsetWitdh +10;// largura do item + margem
+
+botaoProximo.addEventListener("click",()=>{
+    if(contador <carrosselItem.length -1){
+        contador++;
+        carrosselLista.style.transform =`translateX(-${contador * larguraItem}px)`;
+    }
+})
+
+botaoAnterior.addEventListener("click",()=>{
+    if(contador > 0){
+        contador--;
+        carrosselLista.style.transform =`translateX(-${contador * larguraItem}px)`;
+    }
+})
